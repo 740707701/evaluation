34f6c09c87f78051ee3149189dc860c3827c38ea
从从 <740707701@qq.com>
2018-04-26 18:07:37 +0800

    resume page

--- src/views/resume.vue
+++ src/views/resume.vue
@@ -92,12 +92,14 @@
                     <span>
                       <i class="el-icon-bell"></i>12343546546899
                     </span>
-                    <span class="more">
-                      更多<i class="el-icon-arrow-down"></i>
+                    <span class="more" @click="showMoreBase=!showMoreBase">
+                      {{showMoreBase?'收起':'更多'}}
+                      <i v-if="showMoreBase==false" class="el-icon-arrow-down"></i>
+                      <i v-if="showMoreBase==true" class="el-icon-arrow-up"></i>
                     </span>
                   </div>
                 </div>
-                <div class="more-info">
+                <div class="more-info" v-if="showMoreBase">
                   <span class="arrow-top"></span>
                   <span>户口/国籍：上海市</span>
                   <span>婚姻状态：未婚</span>
@@ -134,7 +136,7 @@
                     <span class="name">行业： </span>互联网/电子商务
                   </span>
                   <span class="item">
-                    <span class="name">到岗时间  </span>一周之内
+                    <span class="name">到岗时间： </span>一周之内
                   </span>
                 </li>
               </ul>
@@ -144,7 +146,83 @@
             <div class="base-info">
               <div class="title">
                 <i class="el-icon-bell"></i>
+                <span>自我评价</span>
+              </div>
+              <ul class="evaluate">
+                <li>1.HR可根据求职者经历过的公司来判断该求职者适合的工作氛围及已习得的工作方式，国企、上市公司、私企等的作息、工作饱和度均不同，若此公司为创业型公司，
+                  对一直在国企的员工求职也会考虑该求职者的抗压能力及稳定性。</li>
+                <li>2.若公司全称不为人所熟知，但商标名称是大众所熟知的，可以使用商标名称，让HR可以对求职者所在行业能有一定的了解，以此判断该求职者是否为同行业或跨行业求职</li>
+              </ul>
+            </div>
+          </div>
+          <div class="grid-content info-box">
+            <div class="base-info">
+              <div class="title">
+                <i class="el-icon-bell"></i>
                 <span>工作经验</span>
+                <div class="add-job">
+                  <i class="el-icon-bell"></i>新增工作
+                </div>
+              </div>
+              <div class="job-item item-first">
+                <div class="job-time">
+                  <span class="gray">2018/3-2018/5</span>
+                  <span>上海扶城金融公司</span>
+                  <span>市场销售</span>
+                  <span class="icon-box">
+                    <i class="el-icon-edit"></i>
+                    <i class="el-icon-delete"></i>
+                  </span>
+                </div>
+                <div class="job-type">
+                  <span><span class="gray">公司类型：</span>互联网/教育</span>
+                  <span><span class="gray">公司人数：</span>50-100人</span>
+                  <span><span class="gray">公司性质：</span>民营企业</span>
+                  <span><span class="gray">所属部门：</span>市场部</span>
+                </div>
+                <div class="job-desc">
+                  <div class="desc-text gray">工作描述：</div>
+                  <div class="desc-content">
+                    <ul class="desc-list">
+                      <li class="desc-item">1、参与讨论对项目交互设计、界面结构、操作流程等撰写相关需求文档；</li>
+                      <li class="desc-item">2、负责对App （B端＋C端）整体风格的定位，对用户视觉感受的整体把握和设计</li>
+                      <li class="desc-item">3、负责好车伯乐web网站（前台＋后台）设计页面，整体表现风格的定位，对用户视觉感受的整体把握和设计</li>
+                      <li class="desc-item">4、整理产品需求文档 撰写文档</li>
+                      <li class="desc-item">5、制作各类活动广告设计（画册、单页、易拉宝、海报）</li>
+                      <li class="desc-item">6、协助开发人员页面设计的调整与交互动画讨论</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div class="job-item">
+                <div class="job-time">
+                  <span class="gray">2018/3-2018/5</span>
+                  <span>上海扶城金融公司</span>
+                  <span>市场销售</span>
+                  <span class="icon-box">
+                    <i class="el-icon-edit"></i>
+                    <i class="el-icon-delete"></i>
+                  </span>
+                </div>
+                <div class="job-type">
+                  <span><span class="gray">公司类型：</span>互联网/教育</span>
+                  <span><span class="gray">公司人数：</span>50-100人</span>
+                  <span><span class="gray">公司性质：</span>民营企业</span>
+                  <span><span class="gray">所属部门：</span>市场部</span>
+                </div>
+                <div class="job-desc">
+                  <div class="desc-text gray">工作描述：</div>
+                  <div class="desc-content">
+                    <ul class="desc-list">
+                      <li class="desc-item">1、参与讨论对项目交互设计、界面结构、操作流程等撰写相关需求文档；</li>
+                      <li class="desc-item">2、负责对App （B端＋C端）整体风格的定位，对用户视觉感受的整体把握和设计</li>
+                      <li class="desc-item">3、负责好车伯乐web网站（前台＋后台）设计页面，整体表现风格的定位，对用户视觉感受的整体把握和设计</li>
+                      <li class="desc-item">4、整理产品需求文档 撰写文档</li>
+                      <li class="desc-item">5、制作各类活动广告设计（画册、单页、易拉宝、海报）</li>
+                      <li class="desc-item">6、协助开发人员页面设计的调整与交互动画讨论</li>
+                    </ul>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -153,6 +231,67 @@
               <div class="title">
                 <i class="el-icon-bell"></i>
                 <span>教育经历</span>
+                 <div class="add-job">
+                  <i class="el-icon-bell"></i>新增教育
+                </div>
+              </div>
+              <div class="job-item item-first">
+                <div class="job-time">
+                  <span class="gray">2012/9-2015/7</span>
+                  <span>上海交通大学</span>
+                  <span>视觉传达艺术设计（本科）</span>
+                  <span class="icon-box">
+                    <i class="el-icon-edit"></i>
+                    <i class="el-icon-delete"></i>
+                  </span>
+                </div>
+                <div class="job-desc">
+                  <div class="desc-text gray">专业描述：</div>
+                  <div class="desc-content">
+                    <ul class="desc-list">
+                      <li class="desc-item">运动学规律与制作 3D建模 动画制作等等，有绘画功底</li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="job-desc">
+                  <div class="desc-text gray">主修课程：</div>
+                  <div class="desc-content">
+                    <ul class="desc-list">
+                      <li class="desc-item">1.HR可根据求职者经历过的公司来判断该求职者适合的工作氛围及已习得的工作方式，国企、上市公司、私企等的作息、工作饱和度均不同，
+                        若此公司为创业型公司，对一直在国企的员工求职也会考虑该求职者的抗压能力及稳定性。</li>
+                      <li class="desc-item">2.若公司全称不为人所熟知，但商标名称是大众所熟知的，可以使用商标名称，让HR可以对求职者所在行业能有一定的了解，以此判断该求职者是否为同行业或跨行业求职</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div class="job-item">
+                <div class="job-time">
+                  <span class="gray">2012/9-2015/7</span>
+                  <span>上海交通大学</span>
+                  <span>视觉传达艺术设计（本科）</span>
+                  <span class="icon-box">
+                    <i class="el-icon-edit"></i>
+                    <i class="el-icon-delete"></i>
+                  </span>
+                </div>
+                <div class="job-desc">
+                  <div class="desc-text gray">专业描述：</div>
+                  <div class="desc-content">
+                    <ul class="desc-list">
+                      <li class="desc-item">运动学规律与制作 3D建模 动画制作等等，有绘画功底</li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="job-desc">
+                  <div class="desc-text gray">主修课程：</div>
+                  <div class="desc-content">
+                    <ul class="desc-list">
+                      <li class="desc-item">1.HR可根据求职者经历过的公司来判断该求职者适合的工作氛围及已习得的工作方式，国企、上市公司、私企等的作息、工作饱和度均不同，
+                        若此公司为创业型公司，对一直在国企的员工求职也会考虑该求职者的抗压能力及稳定性。</li>
+                      <li class="desc-item">2.若公司全称不为人所熟知，但商标名称是大众所熟知的，可以使用商标名称，让HR可以对求职者所在行业能有一定的了解，以此判断该求职者是否为同行业或跨行业求职</li>
+                    </ul>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -162,6 +301,86 @@
                 <i class="el-icon-bell"></i>
                 <span>在校情况</span>
               </div>
+              <div class="honor">
+                <div class="school-type">
+                  <span>校内荣誉</span>
+                  <div class="add-job">
+                    <i class="el-icon-bell"></i>新增荣誉
+                  </div>
+                </div>
+                <ul class="honor-list job-item">
+                  <li class="">
+                    <div class="job-time">
+                      <span class="gray">2013/4</span>
+                      <span>微视频比赛</span>
+                      <span>荣获全省“第二名”</span>
+                      <span class="icon-box">
+                        <i class="el-icon-edit"></i>
+                        <i class="el-icon-delete"></i>
+                      </span>
+                    </div>
+                  </li>
+                  <li class="">
+                    <div class="job-time">
+                      <span class="gray">2013/4</span>
+                      <span>微视频比赛2</span>
+                      <span>荣获全省“第二名”</span>
+                      <span class="icon-box">
+                        <i class="el-icon-edit"></i>
+                        <i class="el-icon-delete"></i>
+                      </span>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <div class="post">
+                <div class="school-type">
+                  <span>校内职务</span>
+                  <div class="add-job">
+                    <i class="el-icon-bell"></i>新增职务
+                  </div>
+                </div>
+                <ul class="post-list ">
+                  <li class="job-item">
+                    <div class="job-time">
+                      <span class="gray">2013/4-2014/6</span>
+                      <span>学生会主席</span>
+                      <span></span>
+                      <span></span>
+                    </div>
+                    <div class="job-desc">
+                      <div class="desc-text gray">主修课程：</div>
+                      <div class="desc-content">
+                        <ul class="desc-list">
+                          <li>1.HR可根据求职者经历过的公司来判断该求职者适合的工作氛围及已习得的工作方式，国企、上市公司、私企等的作息、工作饱和度均不同，
+                            若此公司为创业型公司，对一直在国企的员工求职也会考虑该求职者的抗压能力及稳定性。</li>
+                          <li>2.若公司全称不为人所熟知，但商标名称是大众所熟知的，可以使用商标名称，让HR可以对求职者所在行业能有一定的了解，
+                            以此判断该求职者是否为同行业或跨行业求职</li>
+                        </ul>
+                      </div>
+                    </div>
+                  </li>
+                  <li class="job-item">
+                    <div class="job-time">
+                      <span class="gray">2013/4-2014/6</span>
+                      <span>学生会主席</span>
+                      <span></span>
+                      <span></span>
+                    </div>
+                    <div class="job-desc">
+                      <div class="desc-text gray">主修课程：</div>
+                      <div class="desc-content">
+                        <ul class="desc-list">
+                          <li>1.HR可根据求职者经历过的公司来判断该求职者适合的工作氛围及已习得的工作方式，国企、上市公司、私企等的作息、工作饱和度均不同，
+                            若此公司为创业型公司，对一直在国企的员工求职也会考虑该求职者的抗压能力及稳定性。</li>
+                          <li>2.若公司全称不为人所熟知，但商标名称是大众所熟知的，可以使用商标名称，让HR可以对求职者所在行业能有一定的了解，
+                            以此判断该求职者是否为同行业或跨行业求职</li>
+                        </ul>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
           <div class="grid-content info-box">
@@ -169,17 +388,30 @@
               <div class="title">
                 <i class="el-icon-bell"></i>
                 <span>技能证书</span>
+                <div class="add-job">
+                  <i class="el-icon-bell"></i>新增证书
+                </div>
               </div>
-              <div class="skill"></div>
-            </div>
-          </div>
-          <div class="grid-content info-box">
-            <div class="base-info">
-              <div class="title">
-                <i class="el-icon-bell"></i>
-                <span>自我评价</span>
-              </div>
-              <div class="evaluate"></div>
+              <ul class="skill">
+                <li>
+                  <span class="gray">2009/12</span>
+                  <span>市场销售精英证</span>
+                  <span>合格</span>
+                  <span class="icon-box">
+                    <i class="el-icon-edit"></i>
+                    <i class="el-icon-delete"></i>
+                  </span>
+                </li>
+                <li>
+                  <span class="gray">2013/12</span>
+                  <span>市场销售高级证书</span>
+                  <span>合格</span>
+                  <span class="icon-box">
+                    <i class="el-icon-edit"></i>
+                    <i class="el-icon-delete"></i>
+                  </span>
+                </li>
+              </ul>
             </div>
           </div>
         </el-col>
@@ -285,7 +517,7 @@ export default {
   name: "resume",
   data() {
     return {
-      msg: "this is resume page"
+      showMoreBase: false
     };
   },
   computed: {},
@@ -306,6 +538,9 @@ export default {
     padding-top: 7px;
     color: @main-color-yellow;
     cursor: pointer;
+  }
+  .gray {
+    color: @main-color-gray;
   }
   .container {
     width: 1200px;
@@ -381,10 +616,11 @@ export default {
         }
       }
       .base-info {
-        padding: 10px 20px;
+        padding: 10px 0px;
         border-radius: 10px;
         .title {
           line-height: 30px;
+          padding: 0 20px;
           i {
             color: @main-color-yellow;
           }
@@ -393,19 +629,20 @@ export default {
           }
           span {
             color: @main-color-gray;
-            padding-left: 20px;
+            padding-left: 10px;
           }
         }
         .base-content {
           width: 100%;
           display: inline-block;
-          margin: 5px 0;
+          padding: 5px 20px;
+          box-sizing: border-box;
           .avtar {
             float: left;
-            width:85px;
+            width: 85px;
             height: 85px;
             img {
-              width:85px;
+              width: 85px;
               height: 85px;
             }
           }
@@ -429,28 +666,28 @@ export default {
               display: flex;
               display: -webkit-flex;
               line-height: 28px;
-               span {
-                 flex: 1 1 auto;
-               }
-               i {
-                 margin-right: 5px;
-               }
-               .more {
-                 text-align: right;
-                 color: @main-color-blue;
-                 cursor: pointer;
-                 i {
-                   padding-left: 5px;
-                   color: @main-color-gray;
-                 }
-               }
+              span {
+                flex: 1 1 auto;
+              }
+              i {
+                margin-right: 5px;
+              }
+              .more {
+                text-align: right;
+                color: @main-color-blue;
+                cursor: pointer;
+                i {
+                  padding-left: 5px;
+                  color: @main-color-gray;
+                }
+              }
             }
           }
           .more-info {
             line-height: 40px;
             margin-top: 10px;
             padding: 0 15px;
-            background-color: rgba(234,240,253,1);
+            background-color: rgba(234, 240, 253, 1);
             display: flex;
             display: -webkit-flex;
             position: relative;
@@ -458,12 +695,12 @@ export default {
               flex: 1 1 auto;
             }
             .arrow-top {
-              width:0;
+              width: 0;
               height: 0;
               border-top: 10px solid transparent;
               border-left: 10px solid transparent;
               border-right: 10px solid transparent;
-              border-bottom: 10px solid rgba(234,240,253,1);
+              border-bottom: 10px solid rgba(234, 240, 253, 1);
               position: absolute;
               top: -19px;
               right: 25px;
@@ -473,6 +710,8 @@ export default {
         .item-list {
           width: 100%;
           display: inline-block;
+          box-sizing: border-box;
+          padding: 0 20px;
           li {
             line-height: 30px;
             display: flex;
@@ -482,10 +721,99 @@ export default {
             }
             .name {
               width: 90px;
-              letter-spacing: 2px;
               color: @main-color-gray;
               display: inline-block;
             }
+          }
+        }
+        .job-item:last-child {
+          border: none;
+        }
+        .item-first {
+          margin-top: -10px;
+        }
+        .add-job {
+          float: right;
+          color: @main-color-blue;
+          i {
+            margin-right: 6px;
+          }
+        }
+        .icon-box {
+          color: @main-color-blue;
+          text-align: right;
+          i {
+            cursor: pointer;
+          }
+          i:first-child {
+            margin-right: 10px;
+          }
+        }
+        .job-item {
+          display: inline-block;
+          padding: 10px 20px;
+          border-bottom: 1px solid @main-color-border;
+          .job-time,
+          .job-type {
+            display: flex;
+            line-height: 30px;
+            span {
+              flex: 1 1 auto;
+            }
+          }
+          .job-desc {
+            width: 100%;
+            display: inline-block;
+            padding-top: 5px;
+            .desc-text {
+              float: left;
+              width: 70px;
+              line-height: 26px;
+            }
+            .desc-content {
+              margin-left: 80px;
+              .desc-list {
+                li {
+                  line-height: 26px;
+                }
+              }
+            }
+          }
+        }
+        .skill {
+          width: 100%;
+          display: inline-block;
+          padding: 5px 20px;
+          box-sizing: border-box;
+          li {
+            width: 100%;
+            display: flex;
+            line-height: 30px;
+            span {
+              flex: 1 1 auto;
+              max-width: 25%;
+            }
+          }
+        }
+        .honor {
+          padding-top: 10px;
+        }
+        .honor,
+        .post {
+          .school-type {
+            padding: 0 20px;
+            line-height: 30px;
+            background-color: RGBA(247, 247, 247, 1);
+          }
+          .honor-list {
+            width: 100%;
+            box-sizing: border-box;
+          }
+        }
+        .evaluate {
+          padding: 5px 20px;
+          li {
+            line-height: 26px;
           }
         }
       }
