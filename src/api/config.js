69c02563eaf83825f71d5cb58e545e1f4548c0cd
从从 <740707701@qq.com>
2018-05-09 18:15:59 +0800

    store

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,21 @@
 let Apiurl = 'http://192.168.0.173:8088/';
 let url = {
-  cepinglist: 'ceping/cepinglist',
+  //测评
+  cepingList: 'ceping/cepinglist',
+  cepingInfo: 'ceping/cepinginfo',
+  hotlist: 'ceping/hotlist',
+  caichu: 'ceping/tocaichu',
+  //简历
+  baseInfo: 'resume/baseinfo', 
+  evaluate: 'resume/expect',
+  expect: 'resume/expect',
+  edu: 'resume/edu',
+  honor: 'resume/honor',
+  jobexp: 'resume/jobexp',
+  schoolwork: 'resume/schoolwork',
+  skills: 'resume/skills',
+  submitResume: 'resume/submit', //提交个人简历
+  viewResume: 'resume/info', //查看个人简历
 }
 export default {
   Apiurl,
--- src/api/interceptors.js
+++ src/api/interceptors.js
@@ -1,16 +1,21 @@
 import axios from 'axios'
+import store from '../store/index'
 import router from '../router/index'
 
 let AUTH_TOKEN = ''
 
 //axios 默认值
-axios.defaults.baseURL = 'https://api.example.com';
+axios.defaults.timeout = 5000
+axios.defaults.baseURL = 'http://139.196.104.246:8089/';
 axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 //http request 拦截器
 axios.interceptors.request.use(config => {
   config.headers["If-Modified-Since"] = 0;
+  if(store.getters.getToken){
+    config.headers.Authorization = `Token ${store,grtters.getToken}`
+  }
   return config
 }, err => {
   return Promise.reject(err)
@@ -23,7 +28,7 @@ axios.interceptors.response.use(function (response) {
   if(error.response){
     switch(error.response.status){
       case 401:
-      // store.commit('logout')
+      store.commit('logout')
       router.replace({
         path: 'login',
         query: {redirect: router.currentRoute.fullPath}
