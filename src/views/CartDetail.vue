344eb39343de6c8a82a08b6431960b7b49dede8c
cc <740707701@qq.com>
2018-06-26 10:48:39 +0800

    cart

--- src/views/CartDetail.vue
+++ src/views/CartDetail.vue
@@ -15,8 +15,8 @@
           <thead>
             <tr>
               <td style="width:80px">
-                <div @click="checkAll($event)">
-                  <input type="checkbox" :checked="isCheckAll" >
+                <div>
+                  <input type="checkbox" :checked="isCheckAll"  @click="checkAll($event)">
                   <span class="checkall">全选</span>
                 </div>
               </td>
@@ -51,23 +51,24 @@
   </div>
 </template>
 <script>
-import headerNav from '../components/HeaderNav'
-  export default {
-    name: 'cart',
-    data () {
-      return {
-        cepingList: [],
-        totalPrice: 0,
-        isCheckAll: false,
-        checkedIdList: [],
-      }
-    },
-    created (){
-      this.getEvaluationList(2);
-    },
-    methods: {
-      getEvaluationList: function(index) {
-        this.$store
+import headerNav from "../components/HeaderNav";
+export default {
+  name: "cart",
+  data() {
+    return {
+      cepingList: [],
+      totalPrice: 0,
+      isCheckAll: false,
+      isChecked: false,
+      checkedIdList: []
+    };
+  },
+  created() {
+    this.getEvaluationList(2);
+  },
+  methods: {
+    getEvaluationList: function(index) {
+      this.$store
         .dispatch("EVALUATION_LIST", { cepingItem: index })
         .then(res => {
           this.cepingList = res.data;
@@ -85,150 +86,158 @@ import headerNav from '../components/HeaderNav'
             });
           }
         });
-      },
-      //全选
-      checkAll(e){
-        console.log(e.target)
-        var checkAllItem = e.target
-        this.checkedIdList = []
-        if(checkAllItem.checked){
-          for(let item of this.cepingList){
-            item.isChecked = true
-            this.checkedIdList.push(item.id)
-            this.totalPrice += Number(item.price)
-          }
-          this.isCheckAll = true
-        }else {
-          for(let item of this.cepingList){
-            item.isChecked = false
-            this.totalPrice = 0
-          }
-          this.isCheckAll = false
-          this.checkedIdList = []
-        }
-        console.log('cepingList',this.cepingList)
-        console.log('checkIdList',this.checkedIdList)
-      },
-      //单选
-      checkOne(e, id){
-        console.log(e.target, e.target.checked, id)
-        var checkOneItem = e.target
-        if(checkOneItem.checked){
-          if(this.checkedIdList.indexOf(id) == -1){
-            this.checkedIdList.push(id)
-          }
-          if(this.checkedIdList.length == this.cepingList.length){
-            this.isCheckAll = true
-          }
-        }else {
-          this.isCheckAll = false
-          let index = this.checkedIdList.indexOf(id)
-          this.checkedIdList.splice(index, 1)
-        }
-         console.log('checkIdList',this.checkedIdList)
-      },
-      del(id){
-        console.log(id)
-      }
     },
-    components: {
-      headerNav
+    //全选
+    checkAll(e) {
+      var checkAllItem = e.target;
+      this.checkedIdList = [];
+      if (checkAllItem.checked) {
+        for (let item of this.cepingList) {
+          item.isChecked = true;
+          this.checkedIdList.push(item.id);
+          this.totalPrice += Number(item.price);
+        }
+        this.isCheckAll = true;
+      } else {
+        for (let item of this.cepingList) {
+          item.isChecked = false;
+        }
+        this.totalPrice = 0;
+        this.isCheckAll = false;
+        this.checkedIdList = [];
+      }
+      // console.log("cepingList", this.cepingList);
+      // console.log("checkIdList", this.checkedIdList);
+    },
+    //单选
+    checkOne(e, id){
+      let checkOneItem = e.target;
+      if(checkOneItem.checked == true){
+        for(var item of this.cepingList){
+          if(item.id == id){
+            this.checkedIdList.push(item.id)
+            item.isChecked = true
+          }
+        }
+        if(this.cepingList.length == this.checkedIdList.length){
+          this.isCheckAll = true;
+        }
+      }else {
+        for(var item of this.cepingList){
+          if(item.id == id){
+            item.isChecked = false;
+            let index = this.checkedIdList.indexOf(id)
+            this.checkedIdList.splice(index, 1)
+          }
+        }
+        this.isCheckAll = false
+      }
+      // console.log("cepingList", this.cepingList);
+      // console.log("checkIdList", this.checkedIdList);
+    },
+
+    del(id) {
+      console.log(id);
     }
+  },
+  components: {
+    headerNav
   }
+};
 </script>
 <style lang="less" scoped>
-  @import '../assets/css/colors.less';
-  .cart-page {
+@import "../assets/css/colors.less";
+.cart-page {
+  width: 100%;
+  min-height: 100%;
+  background-color: @main-color-bg;
+  padding-top: 60px;
+  padding-bottom: 25px;
+  position: relative;
+  z-index: 1;
+  .banner-bg {
     width: 100%;
-    min-height: 100%;
-    background-color: @main-color-bg;
-    padding-top: 60px;
-    padding-bottom: 25px;
-    position: relative;
-    z-index: 1;
-    .banner-bg {
+    height: 150px;
+    margin-top: 0;
+    position: absolute;
+    z-index: -1;
+    img {
       width: 100%;
       height: 150px;
-      margin-top: 0;
-      position: absolute;
-      z-index: -1;
-      img {
-        width: 100%;
-        height: 150px;
+    }
+  }
+  .container {
+    width: 1200px;
+    margin: 0 auto;
+    margin-top: 45px;
+    height: calc(100% - 45px);
+    .top {
+      height: 30px;
+      line-height: 30px;
+      .title {
+        font-size: 30px;
+        display: inline-block;
+      }
+      .sub-title {
+        margin-left: 100px;
+        display: inline-block;
+      }
+      .right {
+        float: right;
       }
     }
-    .container {
+    .intro-box {
       width: 1200px;
+      height: calc(100% - 200px);
       margin: 0 auto;
-      margin-top: 45px;
-      height: calc(100% - 45px);
-      .top {
-        height: 30px;
-        line-height: 30px;
-        .title {
-          font-size: 30px;
-          display: inline-block;
-        }
-        .sub-title {
-          margin-left: 100px;
-          display: inline-block;
-        }
-        .right {
-          float: right;
-        }
-      }
-      .intro-box {
-        width: 1200px;
-        height: calc(100% - 200px);
-        margin: 0 auto;
-        margin-top: 40px;
-        padding: 20px;
-        background-color: #fff;
-        border-radius: 8px;
-        position: absolute;
-        .table {
-          width: 100%;
-          thead {
-            height: 50px;
-            line-height: 50px;
-            .checkall {
-              margin-left: 6px;
-              cursor: pointer;
-            }
+      margin-top: 40px;
+      padding: 20px;
+      background-color: #fff;
+      border-radius: 8px;
+      position: absolute;
+      .table {
+        width: 100%;
+        thead {
+          height: 50px;
+          line-height: 50px;
+          .checkall {
+            margin-left: 6px;
+            cursor: pointer;
           }
-          tbody {
-            tr {
-              td {
-                line-height: 80px;
-              }
-            }
-          }
+        }
+        tbody {
           tr {
-            border-bottom: 1px solid @main-color-border;
             td {
-              color: @main-color-text;
-              text-align: left;
-              padding-left: 20px;
-              vertical-align: middle;
-              padding: 6px 10px;
-              img {
-                float: left;
-                width: 160px;
-                height: 80px;
-                border-radius: 4px;
-              }
-              .name {
-                display: inline-block;
-                margin-left: 20px;
-              }
+              line-height: 80px;
             }
-            .red {
-              color: red;
+          }
+        }
+        tr {
+          border-bottom: 1px solid @main-color-border;
+          td {
+            color: @main-color-text;
+            text-align: left;
+            padding-left: 20px;
+            vertical-align: middle;
+            padding: 6px 10px;
+            img {
+              float: left;
+              width: 160px;
+              height: 80px;
+              border-radius: 4px;
             }
-            .icon-delete {
-              cursor: pointer;
+            .name {
+              display: inline-block;
+              margin-left: 20px;
             }
-            /*
+          }
+          .red {
+            color: red;
+          }
+          .icon-delete {
+            cursor: pointer;
+          }
+          /*
             input[type="checkbox"] {
               width: 20px;
               height: 20px;
@@ -247,39 +256,39 @@ import headerNav from '../components/HeaderNav'
               border: none;
             }
             */
-          }
-          tr:hover {
-            background-color: #fafafa;
+        }
+        tr:hover {
+          background-color: #fafafa;
+        }
+      }
+      .total-box {
+        float: right;
+        margin-top: 20px;
+        margin-bottom: 30px;
+        .total-money {
+          float: left;
+          margin-right: 100px;
+          padding-top: 10px;
+          .red {
+            color: red;
+            font-size: 16px;
+            line-height: 30px;
           }
         }
-        .total-box {
+        .settle-btn {
           float: right;
-          margin-top: 20px;
-          margin-bottom: 30px;
-          .total-money {
-            float: left;
-            margin-right: 100px;
-            padding-top: 10px;
-            .red {
-              color: red;
-              font-size: 16px;
-              line-height: 30px;
-            }
-          }
-          .settle-btn {
-            float: right;
-            width:112px;
-            height:54px;
-            line-height: 54px;
-            text-align: center;
-            background:rgba(219,53,41,1);
-            border-radius:4px;
-            color: #fff;
-            cursor: pointer;
-          }
+          width: 112px;
+          height: 54px;
+          line-height: 54px;
+          text-align: center;
+          background: rgba(219, 53, 41, 1);
+          border-radius: 4px;
+          color: #fff;
+          cursor: pointer;
         }
       }
     }
   }
+}
 </style>
 
